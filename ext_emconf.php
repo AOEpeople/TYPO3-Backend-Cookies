8310577929aa4da1334f66f2545841d2b64fe45a
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-26 11:30:38 +0000

    Set version to 0.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/becookies/trunk@33494 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "becookies".
 #
-# Auto generated 13-05-2010 16:23
+# Auto generated 26-05-2010 13:30
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.4.0-0.0.0',
@@ -39,7 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"cd86";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"bfe5";s:20:"doc/wizard_form.html";s:4:"1089";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"62ba";s:10:"README.txt";s:4:"e98a";s:16:"ext_autoload.php";s:4:"54cc";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"409f";s:14:"ext_tables.sql";s:4:"c6a4";s:38:"classes/class.tx_becookies_request.php";s:4:"b965";s:48:"classes/class.tx_becookies_requestRepository.php";s:4:"7b98";s:40:"hooks/class.tx_becookies_backendHook.php";s:4:"3279";s:41:"hooks/class.tx_becookies_frontendHook.php";s:4:"41fe";}',
+	'suggests' => array(
+	),
 );
 
 ?>
