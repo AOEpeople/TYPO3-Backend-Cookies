d27e928a1fe9ec336c38de1dfd4ff7d27c7edf49
danp <danp@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-04 08:33:04 +0000

    fix "x" output
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/becookies/trunk@36317 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,3 @@ if (!defined ('TYPO3_MODE')) {
 
 tx_becookies_common::initializeFrontendHook();
 $TYPO3_CONF_VARS['SC_OPTIONS']['typo3/backend.php']['constructPostProcess'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/hooks/class.tx_becookies_backendHook.php:tx_becookies_backendHook->process';
-?>
