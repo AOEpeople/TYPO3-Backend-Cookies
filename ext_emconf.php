511b0be043a09878e5406fdb525c38bfcaf9a4c6
danp <danp@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-18 15:42:53 +0000

    change to 4.3 dependency
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/becookies/trunk@36916 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.5',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.4.0-0.0.0',
+			'typo3' => '4.3.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
