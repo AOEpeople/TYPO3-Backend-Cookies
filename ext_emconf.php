9b5a1085c53e47ffda254d8f19099064f9043918
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-06 15:24:52 +0000

    Set version to 0.1.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/becookies/trunk@37727 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "becookies".
 #
-# Auto generated 14-07-2010 16:37
+# Auto generated 06-09-2010 17:24
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.5',
+	'version' => '0.1.7',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.4.0-0.0.0',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"f19a";s:10:"README.txt";s:4:"e98a";s:16:"ext_autoload.php";s:4:"0505";s:12:"ext_icon.gif";s:4:"b4e6";s:17:"ext_localconf.php";s:4:"878a";s:14:"ext_tables.sql";s:4:"c6a4";s:37:"classes/class.tx_becookies_common.php";s:4:"25ab";s:38:"classes/class.tx_becookies_request.php";s:4:"b965";s:48:"classes/class.tx_becookies_requestRepository.php";s:4:"7b98";s:40:"hooks/class.tx_becookies_backendHook.php";s:4:"ac72";s:41:"hooks/class.tx_becookies_frontendHook.php";s:4:"4470";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"bd5a";s:10:"README.txt";s:4:"e98a";s:16:"ext_autoload.php";s:4:"0505";s:12:"ext_icon.gif";s:4:"b4e6";s:17:"ext_localconf.php";s:4:"878a";s:14:"ext_tables.php";s:4:"e95f";s:14:"ext_tables.sql";s:4:"7ac4";s:16:"locallang_db.xml";s:4:"78ef";s:37:"classes/class.tx_becookies_common.php";s:4:"25ab";s:38:"classes/class.tx_becookies_request.php";s:4:"b965";s:48:"classes/class.tx_becookies_requestRepository.php";s:4:"7b98";s:40:"hooks/class.tx_becookies_backendHook.php";s:4:"6567";s:41:"hooks/class.tx_becookies_frontendHook.php";s:4:"4470";}',
 	'suggests' => array(
 	),
 );
