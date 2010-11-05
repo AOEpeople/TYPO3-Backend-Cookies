6d40ab6537c53a3be35ce04cd38c6d382c8b12d8
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-05 13:38:13 +0000

    Set version to 0.1.9
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/becookies/trunk@39878 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "becookies".
 #
-# Auto generated 05-11-2010 12:35
+# Auto generated 05-11-2010 14:37
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.8',
+	'version' => '0.1.9',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.4.0-0.0.0',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"9010";s:10:"README.txt";s:4:"e98a";s:16:"ext_autoload.php";s:4:"0505";s:12:"ext_icon.gif";s:4:"b4e6";s:17:"ext_localconf.php";s:4:"878a";s:14:"ext_tables.php";s:4:"e95f";s:14:"ext_tables.sql";s:4:"7ac4";s:16:"locallang_db.xml";s:4:"78ef";s:37:"classes/class.tx_becookies_common.php";s:4:"864f";s:38:"classes/class.tx_becookies_request.php";s:4:"b965";s:48:"classes/class.tx_becookies_requestRepository.php";s:4:"7b98";s:40:"hooks/class.tx_becookies_backendHook.php";s:4:"6567";s:41:"hooks/class.tx_becookies_frontendHook.php";s:4:"4470";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"5102";s:10:"README.txt";s:4:"2f5c";s:16:"ext_autoload.php";s:4:"0505";s:12:"ext_icon.gif";s:4:"b4e6";s:17:"ext_localconf.php";s:4:"68ed";s:14:"ext_tables.php";s:4:"e95f";s:14:"ext_tables.sql";s:4:"7ac4";s:16:"locallang_db.xml";s:4:"78ef";s:37:"classes/class.tx_becookies_common.php";s:4:"d880";s:38:"classes/class.tx_becookies_request.php";s:4:"b965";s:48:"classes/class.tx_becookies_requestRepository.php";s:4:"3f81";s:25:"compatibility/10869.patch";s:4:"ce74";s:25:"compatibility/14383.patch";s:4:"0d62";s:43:"compatibility/interface.t3lib_singleton.php";s:4:"febe";s:40:"hooks/class.tx_becookies_backendHook.php";s:4:"9750";s:41:"hooks/class.tx_becookies_frontendHook.php";s:4:"4470";}',
 	'suggests' => array(
 	),
 );
