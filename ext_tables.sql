e873636f954f68375349ec1788b161fca3534529
Martin Tepper <hacksch@arcor.de>
2023-11-19 11:12:59 +0100

    Change length of session id, required for TYPO3 11.5.33

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,8 +5,8 @@ CREATE TABLE tx_becookies_domain_model_request (
     uid int(11) NOT NULL auto_increment,
     tstamp int(11) DEFAULT '0' NOT NULL,
     beuser int(11) DEFAULT '0' NOT NULL,
-	session varchar(40) DEFAULT '' NOT NULL,
+	session varchar(190) DEFAULT '' NOT NULL,
 	domain varchar(255) DEFAULT '' NOT NULL,
-    
+
     PRIMARY KEY (uid)
 )	ENGINE=InnoDB;
